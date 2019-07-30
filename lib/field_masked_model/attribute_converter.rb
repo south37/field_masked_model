ddc6d61c72f1d1ecf4e9741149ce05a96a0583f1
Nao Minami <south37777@gmail.com>
2019-07-30 19:12:16 +0900

    Add support of nullable types

--- lib/field_masked_model/attribute_converter.rb
+++ lib/field_masked_model/attribute_converter.rb
@@ -1,12 +1,27 @@
 require "google/protobuf/timestamp_pb"
+require "google/protobuf/wrappers_pb"
 
 module FieldMaskedModel
   module AttributeConverter
+    NULLABLE_TYPES = [
+      Google::Protobuf::DoubleValue,
+      Google::Protobuf::FloatValue,
+      Google::Protobuf::Int64Value,
+      Google::Protobuf::UInt64Value,
+      Google::Protobuf::Int32Value,
+      Google::Protobuf::UInt32Value,
+      Google::Protobuf::BoolValue,
+      Google::Protobuf::StringValue,
+      Google::Protobuf::BytesValue,
+    ].freeze
+
     class << self
       def convert(value)
         case value
         when Google::Protobuf::Timestamp
           timestamp_to_time(value)
+        when *NULLABLE_TYPES
+          value.value
         else
           # TODO(south37) Add conversion logic of other classes
           value
